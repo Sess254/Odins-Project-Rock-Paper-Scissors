0af33f131503cae6fcf95788a7fb18e8c88e73f8
Sess254 <wamaritemoses@gmail.com>
2024-04-06 20:47:50 +0300

    was able to create a playRound function, more testing

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-let playerSelection =  prompt("Whats Your selection? ").toLowerCase();
-
 function getComputersChoice () {
   const choices = [
     "rock",
     "paper",
-    "scissor"
+    "scissors"
   ]
   const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  const winCombo = {
+    rock: 'scissors',
+    paper: 'rock',
+    scissors: 'paper'
+  };
+
+  if (winCombo[playerSelection] === computerSelection){
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection === computerSelection) {
+    return `You Draw`;
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  }
+
+  }
+  
